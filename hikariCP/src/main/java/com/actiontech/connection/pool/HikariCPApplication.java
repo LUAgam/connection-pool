778b92e7c4eb7014c0f8daa85bcbe17c83513e4a
guoaomen <guoaomen@actionsky.com>
2021-09-30 16:03:19 +0800

    init

--- hikariCP/src/main/java/com/actiontech/connection/pool/HikariCPApplication.java
+++ hikariCP/src/main/java/com/actiontech/connection/pool/HikariCPApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DruidApplication {
+public class HikariCPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DruidApplication.class, args);
+        SpringApplication.run(HikariCPApplication.class, args);
     }
-
 
 }
